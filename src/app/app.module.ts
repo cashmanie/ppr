762ebb1756ac75c7e14ba8c39c6d32fd28d3b867
John Cashman <john@cashman.biz>
2017-10-05 00:57:51 +0100

    Rest of First Map Files

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, ApplicationRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { AgmCoreModule } from '@agm/core';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule, 
+    CommonModule,
+    FormsModule,
+    AgmCoreModule.forRoot({
+      apiKey : 'AIzaSyAbfsWUUSQm2dYLH5W4KIomKDBF_F2GIpU'  
+    })
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
