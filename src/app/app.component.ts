7d1bba72003693511f4064247674a9578f92daf9
John Cashman <john@cashman.biz>
2017-10-05 00:48:00 +0100

    First Map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'John JP Cashman';
+  title = 'John Cashman Heytesbury Map';
+  lat: number=53.334684;
+  lng: number=-6.268933;
+  
+
 }
